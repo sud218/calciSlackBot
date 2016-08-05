d03e6d687cf089a1b935a94908a561bebee0219a
Sudhanshu Shekhar <sudhanshu.shekhar@zomato.com>
2016-08-05 03:09:46 -0400

    slack integration phase 1

--- server.js
+++ server.js
@@ -6,8 +6,7 @@
 // // call the packages we need
 var express = require("express");
 var bodyParser = require('body-parser');
-
-// load math.js 
+var RtmClient = require('@slack/client').RtmClient;
 var math = require('mathjs');
 
 var app = express();
@@ -21,6 +20,14 @@ app.set('port', (process.env.PORT || 8080));
 
 app.use(express.static(__dirname + "/public"));
 
+
+// SLACK CONNECTIONS
+var slackToken = process.env.SLACK_APIKEY || '';
+var slackVerificationCode = process.env.SLACK_VERIFICATION_TOKEN || '';
+
+//var rtm = new RtmClient(token, {logLevel: 'debug'});
+//rtm.start();
+
 // ROUTES FOR OUR API
 // // =============================================================================
 var router = express.Router();              // get an instance of the express Router
@@ -31,12 +38,70 @@ router.get('/', function(req, res) {
 });
 
 // slack calculator api
+router.post("/calc/slack", function(req, res) {
+
+    // api auth
+    if (slackToken == req.body.token) {
+        var expression = req.body.text;
+
+        // check if the body is `help`
+        if (expression == 'help' || expression == '') {
+			res.status(200).json({"username": "calcbot",
+    						"mrkdwn": true,
+							"attachments": [
+        						{
+            						"pretext": "*Hi!* Need to do a calculation while doing your work on Slack? Now, no need to leave slack at all!",
+									"author_name": "Sudhanshu Shekhar",
+            						"author_link": "https://www.linkedin.com/in/sudhanshushekhar1",
+            						"author_icon": "https://avatars0.githubusercontent.com/u/7766076?v=3&u=ecc19fccc2e6f1039d7e597b01064c43052fd2bb&s=140",
+            						"title": "try `/calc (2.25 + 76.0) * 18` or simply `/calc 2 + 2`",
+            						"text": "Supports all normal and scientific expressions! In Slack Calculator Documentation <https://slack-calc.herokuapp.com>",
+            						"color": "green"
+        						}
+    						]});
+        } else {
+             try {
+                 var value = math.round(math.eval(expression), 3); 
+				 res.status(200).json({"username": "calcbot",
+    						"mrkdwn": true,
+							"attachments": [
+        						{
+            						"pretext": expression,
+            						"title": value,
+            						"color": "green"
+        						}
+    						]});
+             } catch (err) {
+				 res.status(200).json({"username": "calcbot",
+    						"mrkdwn": true,
+							"attachments": [
+        						{
+            						"pretext": "`" + expression + "` seems to be broken! Would you mind having a second look and try again?",
+            						"title": "NAN",
+            						"color": "warning"
+        						}
+    						]});
+             }
+        }
+    } else {
+				 res.status(200).json({"username": "calcbot",
+    						"mrkdwn": true,
+							"attachments": [
+        						{
+            						"pretext": "Authentication failed",
+            						"title": "NAN",
+            						"color": "danger"
+        						}
+    						]});
+    }
+});
+
 router.post("/calc", function(req, res) {
 
     // api auth
     var headers = req.headers;
     var apiKey = headers['x-igloo-apikey'];
-    if (apiKey != process.env.APIKEY) {
+    if (apiKey == process.env.APIKEY) {
         var expression = req.body.expression;
         try {
             var value = math.round(math.eval(expression), 3); 
